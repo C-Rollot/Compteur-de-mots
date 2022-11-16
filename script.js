5b32bac19b973f9988e004420c2290300a58e4b7
Clem <clement.rollot71@gmail.com>
2022-11-16 11:42:56 +0100

    Words and letters count features working, a few improvements still to do

---
+++ script.js
@@ -0,0 +1,44 @@
+//Fonction de décompte des mots
+function wordCount() {
+    //Récupération des inputs
+    let nom = document.getElementById("nom1").value;
+    let texte = document.getElementById("texte1").value;
+
+    //Récupération de la div "Résultat"
+    let resultatMots = document.getElementById("resultat-mots");
+
+    //Découpage des mots pour les compter
+    let words = texte.split(" ").length;
+
+    //Message indiquant le nombre de mots à l'utilisateur
+    let message = document.createElement("h2");
+    message.textContent = `Bonjour ${nom}, votre texte contient ${words} mot(s) :)`;
+    resultatMots.appendChild(message);
+}
+
+//Fonction de décompte des lettres
+function letterCount() {
+    //Récupération des inputs
+    let nom = document.getElementById("nom2").value;
+    let texte = document.getElementById("texte2").value;
+
+    //Récupération de la div "Résultat"
+    let resultat = document.getElementById("resultat-lettres");
+
+    //Découpage des mots pour les compter
+    let letters = texte.split("").length;
+
+    //Message indiquant le nombre de mots à l'utilisateur
+    let message = document.createElement("h2");
+    message.textContent = `Bonjour ${nom}, votre texte contient ${letters} lettre(s) :)`;
+    resultat.appendChild(message);
+    messageOrNot = true;
+}
+
+//Récupération des boutons
+let submitWords = document.getElementById("submit-words");
+let submitLetters = document.getElementById("submit-letters");
+
+//Event Listener
+submitWords.addEventListener("click", wordCount);
+submitLetters.addEventListener("click", letterCount);
